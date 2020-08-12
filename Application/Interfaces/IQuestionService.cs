06cec7e0c454c2f0d5f37f38cb78148dedef9b26
horoshev <horoshev.artem@yandex.ru>
2020-08-12 20:57:06 +0700

    Update IQuestionService.cs

--- Application/Interfaces/IQuestionService.cs
+++ Application/Interfaces/IQuestionService.cs
@@ -1,9 +1,25 @@
-﻿using Application.Entities;
+﻿using System.Collections.Generic;
+using Application.Dto;
+using Application.Entities;
 
 namespace Application.Interfaces
 {
-    public interface IQuestionService : IQuestionRepository
+    // TODO: Paging and Sorting
+    public interface IQuestionService
     {
-        Question GetRandomQuestion();
+        IEnumerable<Question> GetAll();
+        /// <summary>
+        /// Returns question with provided id
+        /// </summary>
+        /// <param name="entityId">Identifier of the question</param>
+        /// <returns>Founded question of null if question was not found</returns>
+        Question Get(int entityId);
+        Question? Create(QuestionDto entity);
+        Question? Update(QuestionDto entity);
+        Question? Delete(QuestionDto entity);
+        Question? Delete(int entityId);
+
+        Question? GetRandomQuestion();
+        IEnumerable<Question> GetQuestionByAuthorId(string authorId);
     }
 }
